12821768fc8052d042ef2c4393c8cff42f9a0381
Julia Kim <juliaykim6@gmail.com>
2021-01-10 15:39:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,11 @@
 def unsafe?(speed)
-  if speed > 60 || speed < 40 
-    return true 
+  if speed > 60 || speed < 40
+    return true
   elsereturn false 
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
